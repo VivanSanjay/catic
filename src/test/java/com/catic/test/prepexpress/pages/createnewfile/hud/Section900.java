051b11604d6c432165caa9c39ae19fc45853f29a
QT <ariordan@qualitestgroup.com>
2018-07-02 14:43:31 -0400

    adding locators for hud section 900

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section900.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section900.java
@@ -1,7 +1,119 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section900 extends PageObject {
-
+	@FindBy(id = "L901FromDate")
+	private WebElementFacade l901FromDateField;
+	
+	@FindBy(id = "L901ToDate")
+	private WebElementFacade l901ToDateField;
+	
+	@FindBy(id = "L901InterestPerDay")
+	private WebElementFacade l901InterestPerDayField;
+	
+	@FindBy(id = "L901Days")
+	private WebElementFacade l901DaysInputField;
+	
+	@FindBy(id = "btn901Override")
+	private WebElementFacade l901ManualOverrideButton;
+	
+	@FindBy(id = "L901BorrowerAmt")
+	private WebElementFacade l901BorrowerAmountField;
+	
+	@FindBy(id = "L901SellerAmt")
+	private WebElementFacade l901SellerAmountField;
+	
+	@FindBy(id = "L902Premium")
+	private WebElementFacade l902MortgageInsurancePremiumInputField;
+	
+	@FindBy(id = "L902Months")
+	private WebElementFacade l902MonthsInputField;
+	
+	@FindBy(id = "L902Payee")
+	private WebElementFacade l902PayeeInputField;
+	
+	@FindBy(id = "L902POCAmount")
+	private WebElementFacade l902POCAmountInputField;
+	
+	@FindBy(id = "btn902Poc")
+	private WebElementFacade l902POCButton;
+	
+	@FindBy(id = "L902BorrowerAmt")
+	private WebElementFacade l902BorrowerAmountInputField;
+	
+	@FindBy(id = "L902SellerAmt")
+	private WebElementFacade l902SellerAmountInputField;
+	
+	@FindBy(id = "L903Premium")
+	private WebElementFacade l903HomeownersInsuranceInputField;
+	
+	@FindBy(id = "L903Years")
+	private WebElementFacade l903YearsInputField;
+	
+	@FindBy(id = "L903Payee")
+	private WebElementFacade l903PayeeInputField;
+	
+	@FindBy(id = "L903POCAmount")
+	private WebElementFacade l903POCAmountInputField;
+	
+	@FindBy(id = "btn903Poc")
+	private WebElementFacade l903POCButton;
+	
+	@FindBy(id = "L903BorrowerAmt")
+	private WebElementFacade l903BorrowerAmountInputField;
+	
+	@FindBy(id = "L903SellerAmt")
+	private WebElementFacade l903SellerAmountInputField;
+	
+	@FindBy(id = "L904Text")
+	private WebElementFacade l904TextInputField;
+	
+	@FindBy(id = "L904Premium")
+	private WebElementFacade l904PremiumInputField;
+	
+	@FindBy(id = "L904Years")
+	private WebElementFacade l904YearsInputField;
+	
+	@FindBy(id = "L904Payee")
+	private WebElementFacade l904PayeeInputField;
+	
+	@FindBy(id = "L904POCAmount")
+	private WebElementFacade l904POCAmountInputField;
+	
+	@FindBy(id = "btn904Poc")
+	private WebElementFacade l904POCButton;
+	
+	@FindBy(id = "L904BorrowerAmt")
+	private WebElementFacade l904BorrowerAmountInputField;
+	
+	@FindBy(id = "L904SellerAmt")
+	private WebElementFacade l904SellerAmountInputField;
+	
+	@FindBy(id = "L905Text")
+	private WebElementFacade l905TextInputField;
+	
+	@FindBy(id = "btn905Gfe")
+	private WebElementFacade l905GFEButton;
+	
+	@FindBy(id = "L905BorrowerAmt")
+	private WebElementFacade l905BorrowerAmountInputField;
+	
+	@FindBy(id = "L905SellerAmt")
+	private WebElementFacade l905SellerAmountInputField;
+	
+	@FindBy(id = "L906Text")
+	private WebElementFacade l906TextInputField;
+	
+	@FindBy(id = "btn906Gfe")
+	private WebElementFacade l906GFEButton;
+	
+	@FindBy(id = "L906BorrowerAmt")
+	private WebElementFacade l906BorrowerAmountInputField;
+	
+	@FindBy(id = "L906SellerAmt")
+	private WebElementFacade l906SellerAmountInputField;
 }
