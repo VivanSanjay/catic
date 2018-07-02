1f443b494272eeefceea61590ce0d6ec8bfea143
QT <ariordan@qualitestgroup.com>
2018-07-02 13:25:32 -0400

    added locators for hud section 200/500

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section200and500.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section200and500.java
@@ -1,7 +1,303 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section200and500 extends PageObject {
-
+	// 200 - amounts paid by/in behalf of borrower
+	@FindBy(id = "L201Amount")
+	private WebElementFacade l201AmountInputField;
+	
+	@FindBy(id = "L202Text")
+	private WebElementFacade l202TextInputField;
+	
+	@FindBy(id = "L202Amount")
+	private WebElementFacade l202AmountInputField;
+	
+	@FindBy(id = "L202OverrideButton")
+	private WebElementFacade l202ManualOverrideButton;
+	
+	@FindBy(id = "L203Amount")
+	private WebElementFacade l203AmountInputField;
+	
+	@FindBy(id = "L204Text")
+	private WebElementFacade l204InputField;
+	
+	@FindBy(id = "L204Amount")
+	private WebElementFacade l204AmountInputField;
+	
+	@FindBy(id = "L205Text")
+	private WebElementFacade l205InputField;
+	
+	@FindBy(id = "L205Amount")
+	private WebElementFacade l205AmountInputField;
+	
+	@FindBy(id = "L206Text")
+	private WebElementFacade l206InputField;
+	
+	@FindBy(id = "L206Amount")
+	private WebElementFacade l206AmountInputField;
+	
+	@FindBy(id = "L207Text")
+	private WebElementFacade l207InputField;
+	
+	@FindBy(id = "L207Amount")
+	private WebElementFacade l207AmountInputField;
+	
+	@FindBy(id = "L208Text")
+	private WebElementFacade l208InputField;
+	
+	@FindBy(id = "L208Amount")
+	private WebElementFacade l208AmountInputField;
+	
+	@FindBy(id = "L209Text")
+	private WebElementFacade l209InputField;
+	
+	@FindBy(id = "L209Amount")
+	private WebElementFacade l209AmountInputField;
+	
+	// city/town taxes
+	@FindBy(id = "L210FromDate")
+	private WebElementFacade l210FromDateInputField;
+	
+	@FindBy(id = "L210ToDate")
+	private WebElementFacade l210ToDateInputField;
+	
+	@FindBy(id = "L210Amount")
+	private WebElementFacade l210AmountInputField;
+	
+	@FindBy(id = "L210OverrideButton")
+	private WebElementFacade l210ManualOverrideButton;
+	
+	// county taxes
+	@FindBy(id = "L211FromDate")
+	private WebElementFacade l211FromDateInputField;
+	
+	@FindBy(id = "L211ToDate")
+	private WebElementFacade l211ToDateInputField;
+	
+	@FindBy(id = "L211Amount")
+	private WebElementFacade l211AmountInputField;
+	
+	// assessments
+	@FindBy(id = "L212FromDate")
+	private WebElementFacade l212FromDateInputField;
+	
+	@FindBy(id = "L212ToDate")
+	private WebElementFacade l212ToDateInputField;
+	
+	@FindBy(id = "L212Amount")
+	private WebElementFacade l212AmountInputField;
+	
+	// other
+	@FindBy(id = "L213Text")
+	private WebElementFacade l213TextInputField;
+	
+	@FindBy(id = "L213Calc")
+	private WebElementFacade l213CalcButton;
+	
+	@FindBy(id = "L213Amount")
+	private WebElementFacade l213AmountInputField;
+	
+	@FindBy(id = "L213POCButton")
+	private WebElementFacade l213POCButton;
+	
+	@FindBy(id = "L214Text")
+	private WebElementFacade l214TextInputField;
+	
+	@FindBy(id = "L214Calc")
+	private WebElementFacade l214CalcButton;
+	
+	@FindBy(id = "L214Amount")
+	private WebElementFacade l214AmountInputField;
+	
+	@FindBy(id = "L214POCButton")
+	private WebElementFacade l214POCButton;
+	
+	@FindBy(id = "L215Text")
+	private WebElementFacade l215TextInputField;
+	
+	@FindBy(id = "L215Calc")
+	private WebElementFacade l215CalcButton;
+	
+	@FindBy(id = "L215Amount")
+	private WebElementFacade l215AmountInputField;
+	
+	@FindBy(id = "L215POCButton")
+	private WebElementFacade l215POCButton;
+	
+	@FindBy(id = "L216Text")
+	private WebElementFacade l216TextInputField;
+	
+	@FindBy(id = "L216Calc")
+	private WebElementFacade l216CalcButton;
+	
+	@FindBy(id = "L216Amount")
+	private WebElementFacade l216AmountInputField;
+	
+	@FindBy(id = "L216POCButton")
+	private WebElementFacade l216POCButton;
+	
+	@FindBy(id = "L217Text")
+	private WebElementFacade l217TextInputField;
+	
+	@FindBy(id = "L217Calc")
+	private WebElementFacade l217CalcButton;
+	
+	@FindBy(id = "L217Amount")
+	private WebElementFacade l217AmountInputField;
+	
+	@FindBy(id = "L217POCButton")
+	private WebElementFacade l217POCButton;
+	
+	@FindBy(id = "L218Text")
+	private WebElementFacade l218TextInputField;
+	
+	@FindBy(id = "L218Calc")
+	private WebElementFacade l218CalcButton;
+	
+	@FindBy(id = "L218Amount")
+	private WebElementFacade l218AmountInputField;
+	
+	@FindBy(id = "L218POCButton")
+	private WebElementFacade l218POCButton;
+	
+	@FindBy(id = "L219Text")
+	private WebElementFacade l219TextInputField;
+	
+	@FindBy(id = "L219Calc")
+	private WebElementFacade l219CalcButton;
+	
+	@FindBy(id = "L219Amount")
+	private WebElementFacade l219AmountInputField;
+	
+	@FindBy(id = "L219POCButton")
+	private WebElementFacade l219POCButton;
+	
+	@FindBy(id = "L220SectionTotal")
+	private WebElementFacade totalPaidByForBorrowerField;
+	
+	// 500 - reductions in amount due to seller
+	@FindBy(id = "L501Amount")
+	private WebElementFacade l501AmountInputField;
+	
+	@FindBy(id = "L502Amount")
+	private WebElementFacade l502AmountField;
+	
+	@FindBy(id = "L503Amount")
+	private WebElementFacade l503AmountField;
+	
+	@FindBy(id = "L504Text")
+	private WebElementFacade l504TextInputField;
+	
+	@FindBy(id = "L504Amount")
+	private WebElementFacade l504AmountInputField;
+	
+	@FindBy(id = "L505Text")
+	private WebElementFacade l505TextInputField;
+	
+	@FindBy(id = "L505Amount")
+	private WebElementFacade l505AmountInputField;
+	
+	@FindBy(id = "L506Text")
+	private WebElementFacade l506TextInputField;
+	
+	@FindBy(id = "L506Amount")
+	private WebElementFacade l506AmountInputField;
+	
+	@FindBy(id = "L507Text")
+	private WebElementFacade l507TextInputField;
+	
+	@FindBy(id = "L507Amount")
+	private WebElementFacade l507AmountInputField;
+	
+	@FindBy(id = "L508Text")
+	private WebElementFacade l508TextInputField;
+	
+	@FindBy(id = "L508Amount")
+	private WebElementFacade l508AmountInputField;
+	
+	@FindBy(id = "L509Text")
+	private WebElementFacade l509TextInputField;
+	
+	@FindBy(id = "L509Amount")
+	private WebElementFacade l509AmountInputField;
+	
+	// city/town taxes
+	@FindBy(id = "L510FromDate")
+	private WebElementFacade l510FromDateField;
+	
+	@FindBy(id = "L510ToDate")
+	private WebElementFacade l510ToDateField;
+	
+	@FindBy(id = "L510Amount")
+	private WebElementFacade l510AmountField;
+	
+	// county taxes
+	@FindBy(id = "L511FromDate")
+	private WebElementFacade l511FromDateField;
+	
+	@FindBy(id = "L511ToDate")
+	private WebElementFacade l511ToDateField;
+	
+	@FindBy(id = "L511Amount")
+	private WebElementFacade l511AmountField;
+	
+	// assessments
+	@FindBy(id = "L512FromDate")
+	private WebElementFacade l512FromDateField;
+	
+	@FindBy(id = "L512ToDate")
+	private WebElementFacade l512ToDateField;
+	
+	@FindBy(id = "L512Amount")
+	private WebElementFacade l512AmountField;
+	
+	// other
+	@FindBy(id = "L513Text")
+	private WebElementFacade l513TextField;
+	
+	@FindBy(id = "L513Amount")
+	private WebElementFacade l513AmountField;
+	
+	@FindBy(id = "L514Text")
+	private WebElementFacade l514TextField;
+	
+	@FindBy(id = "L514Amount")
+	private WebElementFacade l514AmountField;
+	
+	@FindBy(id = "L515Text")
+	private WebElementFacade l515TextField;
+	
+	@FindBy(id = "L515Amount")
+	private WebElementFacade l515AmountField;
+	
+	@FindBy(id = "L516Text")
+	private WebElementFacade l516TextField;
+	
+	@FindBy(id = "L516Amount")
+	private WebElementFacade l516AmountField;
+	
+	@FindBy(id = "L517Text")
+	private WebElementFacade l517TextField;
+	
+	@FindBy(id = "L517Amount")
+	private WebElementFacade l517AmountField;
+	
+	@FindBy(id = "L518Text")
+	private WebElementFacade l518TextField;
+	
+	@FindBy(id = "L518Amount")
+	private WebElementFacade l518AmountField;
+	
+	@FindBy(id = "L519Text")
+	private WebElementFacade l519TextField;
+	
+	@FindBy(id = "L519Amount")
+	private WebElementFacade l519AmountField;
+	
+	@FindBy(id = "L520SectionTotal")
+	private WebElementFacade totalReductionsAmountDueSellerField;
 }
