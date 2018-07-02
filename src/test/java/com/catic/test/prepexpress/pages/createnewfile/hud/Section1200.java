49c151342bea38419c5f5320de2cf8e79cc60e66
QT <ariordan@qualitestgroup.com>
2018-07-02 16:00:25 -0400

    adding locators for section 1200 on hud form

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1200.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1200.java
@@ -1,7 +1,143 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section1200 extends PageObject {
-
+	@FindBy(id = "L1201BorrowerAmt")
+	private WebElementFacade l1201BorrowerAmountField;
+	
+	@FindBy(id = "L1201SellerAmt")
+	private WebElementFacade l1201SellerAmountField;
+	
+	@FindBy(id = "L1202Calc")
+	private WebElementFacade l1202CalcButton;
+	
+	@FindBy(id = "L1202DeedAmt")
+	private WebElementFacade l1202DeedAmountInputField;
+	
+	@FindBy(id = "btn1202DeedBuySell")
+	private WebElementFacade l1202DeedBuyOrSellButton;
+	
+	@FindBy(id = "L1202MtgAmt")
+	private WebElementFacade l1202MortgageAmountInputField;
+	
+	@FindBy(id = "btn1202MtgBuySell")
+	private WebElementFacade l1202MortgageBuyOrSellButton;
+	
+	@FindBy(id = "L1202ReleaseAmt")
+	private WebElementFacade l1202ReleaseAmountInputField;
+	
+	@FindBy(id = "btn1202ReleaseBuySell")
+	private WebElementFacade l1202ReleaseBuyOrSellButton;
+	
+	@FindBy(id = "L1203OverrideButton")
+	private WebElementFacade l1203ManualOverrideButton;
+	
+	@FindBy(id = "L1203BorrowerAmt")
+	private WebElementFacade l1203BorrowerAmountInputField;
+	
+	@FindBy(id = "L1203SellerAmt")
+	private WebElementFacade l1203SellerAmountInputField;
+	
+	@FindBy(id = "L1204DeedAmt")
+	private WebElementFacade l1204DeedAmountInputField;
+	
+	@FindBy(id = "L1204MtgAmt")
+	private WebElementFacade l1204MortgageAmountField;
+	
+	@FindBy(id = "L1205DeedAmt")
+	private WebElementFacade l1205DeedAmountInputField;
+	
+	@FindBy(id = "L1205MtgAmt")
+	private WebElementFacade l1205MortgageAmountField;
+	
+	@FindBy(id = "L1206Text")
+	private WebElementFacade l1206TextInputField;
+	
+	@FindBy(id = "L1206Amount")
+	private WebElementFacade l1206AmountInputField;
+	
+	@FindBy(id = "btn1206BuySell")
+	private WebElementFacade l1206BuyOrSellButton;
+	
+	@FindBy(xpath = "(//input[@name='options1206'])[1]")
+	private WebElementFacade l1206Option1201RadioButton;
+	
+	@FindBy(xpath = "(//input[@name='options1206'])[2]")
+	private WebElementFacade l1206Option1203RadioButton;
+	
+	@FindBy(id = "L1206AdditionalTax")
+	private WebElementFacade l1206AdditionalTaxCheckbox;
+	
+	@FindBy(id = "L1206BorrowerAmt")
+	private WebElementFacade l1206BorrowerAmountInputField;
+	
+	@FindBy(id = "L1206SellerAmt")
+	private WebElementFacade l1206SellerAmountInputField;
+	
+	@FindBy(id = "L1207Text")
+	private WebElementFacade l1207TextInputField;
+	
+	@FindBy(id = "L1207Amount")
+	private WebElementFacade l1207AmountInputField;
+	
+	@FindBy(id = "btn1207BuySell")
+	private WebElementFacade l1207BuyOrSellButton;
+	
+	@FindBy(xpath = "(//input[@name='options1207'])[1]")
+	private WebElementFacade l1207Option1201RadioButton;
+	
+	@FindBy(xpath = "(//input[@name='options1207'])[2]")
+	private WebElementFacade l1207Option1203RadioButton;
+	
+	@FindBy(id = "L1207BorrowerAmt")
+	private WebElementFacade l1207BorrowerAmountInputField;
+	
+	@FindBy(id = "L1207SellerAmt")
+	private WebElementFacade l1207SellerAmountInputField;
+	
+	@FindBy(id = "L1208Text")
+	private WebElementFacade l1208TextInputField;
+	
+	@FindBy(id = "L1208Amount")
+	private WebElementFacade l1208AmountInputField;
+	
+	@FindBy(id = "btn1208BuySell")
+	private WebElementFacade l1208BuyOrSellButton;
+	
+	@FindBy(xpath = "(//input[@name='options1208'])[1]")
+	private WebElementFacade l1208Option1201RadioButton;
+	
+	@FindBy(xpath = "(//input[@name='options1208'])[2]")
+	private WebElementFacade l1208Option1203RadioButton;
+	
+	@FindBy(id = "L1208BorrowerAmt")
+	private WebElementFacade l1208BorrowerAmountInputField;
+	
+	@FindBy(id = "L1208SellerAmt")
+	private WebElementFacade l1208SellerAmountInputField;
+	
+	@FindBy(id = "L1209Text")
+	private WebElementFacade l1209TextInputField;
+	
+	@FindBy(id = "L1209Amount")
+	private WebElementFacade l1209AmountInputField;
+	
+	@FindBy(id = "btn1209BuySell")
+	private WebElementFacade l1209BuyOrSellButton;
+	
+	@FindBy(xpath = "(//input[@name='options1209'])[1]")
+	private WebElementFacade l1209Option1201RadioButton;
+	
+	@FindBy(xpath = "(//input[@name='options1209'])[2]")
+	private WebElementFacade l1209Option1203RadioButton;
+	
+	@FindBy(id = "L1209BorrowerAmt")
+	private WebElementFacade l1209BorrowerAmountInputField;
+	
+	@FindBy(id = "L1209SellerAmt")
+	private WebElementFacade l1209SellerAmountInputField;
 }
