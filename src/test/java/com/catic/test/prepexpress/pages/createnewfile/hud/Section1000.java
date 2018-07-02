6da163673002b79538974af49320525038e01a7e
QT <ariordan@qualitestgroup.com>
2018-07-02 15:22:33 -0400

    added locators for section 1000 on hud form

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1000.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1000.java
@@ -1,7 +1,149 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section1000 extends PageObject {
-
+	@FindBy(id = "L1001OverrideButton")
+	private WebElementFacade l1001ManualOverrideButton;
+	
+	@FindBy(id = "L1001BorrowerAmt")
+	private WebElementFacade l1001BorrowerAmountInputField;
+	
+	@FindBy(id = "L1001SellerAmt")
+	private WebElementFacade l1001SellerAmountInputField;
+	
+	@FindBy(id = "L1002NumberOfPayments")
+	private WebElementFacade l1002NumberOfPaymentsInputField;
+	
+	@FindBy(id = "L1002PaymentAmt")
+	private WebElementFacade l1002PaymentAmountInputField;
+	
+	@FindBy(id = "L1002Amount")
+	private WebElementFacade l1002AmountInputField;
+	
+	@FindBy(id = "L1002BorrowerAmt")
+	private WebElementFacade l1002BorrowerAmountField;
+	
+	@FindBy(id = "L1002SellerAmt")
+	private WebElementFacade l1002SellerAmountField;
+	
+	@FindBy(id = "L1003NumberOfPayments")
+	private WebElementFacade l1003NumberOfPaymentsInputField;
+	
+	@FindBy(id = "L1003PaymentAmt")
+	private WebElementFacade l1003PaymentAmountInputField;
+	
+	@FindBy(id = "L1003Amount")
+	private WebElementFacade l1003AmountInputField;
+	
+	@FindBy(id = "L1003BorrowerAmt")
+	private WebElementFacade l1003BorrowerAmountField;
+	
+	@FindBy(id = "L1003SellerAmt")
+	private WebElementFacade l1003SellerAmountField;
+	
+	@FindBy(id = "L1004NumberOfPayments")
+	private WebElementFacade l1004NumberOfPaymentsInputField;
+	
+	@FindBy(id = "L1004PaymentAmt")
+	private WebElementFacade l1004PaymentAmountInputField;
+	
+	@FindBy(id = "L1004Amount")
+	private WebElementFacade l1004AmountInputField;
+	
+	@FindBy(id = "L1004BorrowerAmt")
+	private WebElementFacade l1004BorrowerAmountField;
+	
+	@FindBy(id = "L1004SellerAmt")
+	private WebElementFacade l1004SellerAmountField;
+	
+	@FindBy(id = "L1005Text")
+	private WebElementFacade l1005TextInputField;
+	
+	@FindBy(id = "L1005NumberOfPayments")
+	private WebElementFacade l1005NumberOfPaymentsInputField;
+	
+	@FindBy(id = "L1005PaymentAmt")
+	private WebElementFacade l1005PaymentAmountInputField;
+	
+	@FindBy(id = "L1005Amount")
+	private WebElementFacade l1005AmountInputField;
+	
+	@FindBy(id = "L1005BorrowerAmt")
+	private WebElementFacade l1005BorrowerAmountField;
+	
+	@FindBy(id = "L1005SellerAmt")
+	private WebElementFacade l1005SellerAmountField;
+	
+	@FindBy(id = "L1006Text")
+	private WebElementFacade l1006TextInputField;
+	
+	@FindBy(id = "L1006NumberOfPayments")
+	private WebElementFacade l1006NumberOfPaymentsInputField;
+	
+	@FindBy(id = "L1006PaymentAmt")
+	private WebElementFacade l1006PaymentAmountInputField;
+	
+	@FindBy(id = "L1006Amount")
+	private WebElementFacade l1006AmountInputField;
+	
+	@FindBy(id = "L1006BorrowerAmt")
+	private WebElementFacade l1006BorrowerAmountField;
+	
+	@FindBy(id = "L1006SellerAmt")
+	private WebElementFacade l1006SellerAmountField;
+	
+	@FindBy(id = "L1007Text")
+	private WebElementFacade l1007TextInputField;
+	
+	@FindBy(id = "L1007NumberOfPayments")
+	private WebElementFacade l1007NumberOfPaymentsInputField;
+	
+	@FindBy(id = "L1007PaymentAmt")
+	private WebElementFacade l1007PaymentAmountInputField;
+	
+	@FindBy(id = "L1007Amount")
+	private WebElementFacade l1007AmountInputField;
+	
+	@FindBy(id = "L1007BorrowerAmt")
+	private WebElementFacade l1007BorrowerAmountField;
+	
+	@FindBy(id = "L1007SellerAmt")
+	private WebElementFacade l1007SellerAmountField;
+	
+	@FindBy(id = "L1008Text")
+	private WebElementFacade l1008TextField;
+	
+	@FindBy(id = "L1008Amount")
+	private WebElementFacade l1008AmountInputField;
+	
+	@FindBy(id = "L1008BorrowerAmt")
+	private WebElementFacade l1008BorrowerAmountField;
+	
+	@FindBy(id = "L1008SellerAmt")
+	private WebElementFacade l1008SellerAmountField;
+	
+	@FindBy(id = "L1009Text")
+	private WebElementFacade l1009TextInputField;
+	
+	@FindBy(id = "L1009Amount")
+	private WebElementFacade l1009AmountInputField;
+	
+	@FindBy(id = "L1009BorrowerAmt")
+	private WebElementFacade l1009BorrowerAmountField;
+	
+	@FindBy(id = "L1009SellerAmt")
+	private WebElementFacade l1009SellerAmountField;
+	
+	@FindBy(id = "L1008AggregateAdjustment1008")
+	private WebElementFacade aggregateAdjustment1008RadioButton;
+	
+	@FindBy(id = "L1009AggregateAdjustment1009")
+	private WebElementFacade aggregateAdjustment1009RadioButton;
+	
+	@FindBy(id = "DisplayAsNegative1000")
+	private WebElementFacade displayAsNegativeNumberCheckbox;
 }
