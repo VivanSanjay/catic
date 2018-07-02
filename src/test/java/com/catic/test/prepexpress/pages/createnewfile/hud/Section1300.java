bc65a6664d6c0b70b7e852bc68cd8df363f2a13a
QT <ariordan@qualitestgroup.com>
2018-07-02 13:57:38 -0400

    added locators for hud section 1300 and 1400

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1300.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1300.java
@@ -1,7 +1,92 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section1300 extends PageObject {
-
+	@FindBy(id = "L1301RollUpGFE")
+	private WebElementFacade l1301RollupGFECheckbox;
+	
+	@FindBy(id = "L1301BorrowerAmt")
+	private WebElementFacade l1301BorrowerAmountField;
+	
+	@FindBy(id = "L1301SellerAmt")
+	private WebElementFacade l1301SellerAmountField;
+	
+	@FindBy(id = "L1302LineNo")
+	private WebElementFacade l1302LineNoInputField;
+	
+	@FindBy(id = "L1302Text")
+	private WebElementFacade l1302TextInputField;
+	
+	@FindBy(id = "L1302Amount")
+	private WebElementFacade l1302AmountInputField;
+	
+	@FindBy(id = "btn1302Gfe")
+	private WebElementFacade l1302GfeButton;
+	
+	@FindBy(id = "L1302BorrowerAmt")
+	private WebElementFacade l1302BorrowerAmountInputField;
+	
+	@FindBy(id = "L1302SellerAmt")
+	private WebElementFacade l1302SellerAmountInputField;
+	
+	@FindBy(id = "L1303LineNo")
+	private WebElementFacade l1303LineNoInputField;
+	
+	@FindBy(id = "L1303Text")
+	private WebElementFacade l1303TextInputField;
+	
+	@FindBy(id = "L1303Amount")
+	private WebElementFacade l1303AmountInputField;
+	
+	@FindBy(id = "btn1303Gfe")
+	private WebElementFacade l1303GfeButton;
+	
+	@FindBy(id = "L1303BorrowerAmt")
+	private WebElementFacade l1303BorrowerAmountInputField;
+	
+	@FindBy(id = "L1303SellerAmt")
+	private WebElementFacade l1303SellerAmountInputField;
+	
+	@FindBy(id = "L1304LineNo")
+	private WebElementFacade l1304LineNoInputField;
+	
+	@FindBy(id = "L1304Text")
+	private WebElementFacade l1304TextInputField;
+	
+	@FindBy(id = "L1304Amount")
+	private WebElementFacade l1304AmountInputField;
+	
+	@FindBy(id = "btn1304Gfe")
+	private WebElementFacade l1304GfeButton;
+	
+	@FindBy(id = "L1304BorrowerAmt")
+	private WebElementFacade l1304BorrowerAmountInputField;
+	
+	@FindBy(id = "L1304SellerAmt")
+	private WebElementFacade l1304SellerAmountInputField;
+	
+	@FindBy(id = "L1305LineNo")
+	private WebElementFacade l1305LineNoInputField;
+	
+	@FindBy(id = "L1305Text")
+	private WebElementFacade l1305TextField;
+	
+	@FindBy(id = "L1305Amount")
+	private WebElementFacade l1305AmountField;
+	
+	@FindBy(id = "btn1305Gfe")
+	private WebElementFacade l1305GfeButton;
+	
+	@FindBy(id = "L1305BorrowerAmt")
+	private WebElementFacade l1305BorrowerAmountField;
+	
+	@FindBy(id = "L1305SellerAmt")
+	private WebElementFacade l1305SellerAmountField;
+	
+	@FindBy(id = "btnOpen1300Continuation")
+	private WebElementFacade add1300ContinuationButton;
 }
--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1400.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1400.java
@@ -1,7 +1,14 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section1400 extends PageObject {
-
+	@FindBy(id = "L1400BorrowerAmt")
+	private WebElementFacade l1400BorrowerAmountField;
+	
+	@FindBy(id = "L1400SellerAmt")
+	private WebElementFacade l1400SellerAmountField;
 }
