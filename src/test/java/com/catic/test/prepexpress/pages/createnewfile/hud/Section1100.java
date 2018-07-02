33adb1e10d46cc4d3d2c40f7625efa2ddb2f3c96
QT <ariordan@qualitestgroup.com>
2018-07-02 15:41:52 -0400

    adding locators for section 1100 hud form

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1100.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/hud/Section1100.java
@@ -1,7 +1,140 @@
 package com.catic.test.prepexpress.pages.createnewfile.hud;
 
+import org.openqa.selenium.support.FindBy;
+
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 
 public class Section1100 extends PageObject {
-
+	@FindBy(id = "L1101RollUpGFE")
+	private WebElementFacade l1101RollupGFECheckbox;
+	
+	@FindBy(id = "btn1101Gfe")
+	private WebElementFacade l1101GfeButton;
+	
+	@FindBy(id = "L1101OverrideButton")
+	private WebElementFacade l1101ManualOverrideButton;
+	
+	@FindBy(id = "L1101BorrowerAmt")
+	private WebElementFacade l1101BorrowerAmountInputField;
+	
+	@FindBy(id = "L1101SellerAmt")
+	private WebElementFacade l1101SellerAmountInputField;
+	
+	@FindBy(id = "L1102Text")
+	private WebElementFacade l1102TextInputField;
+	
+	@FindBy(id = "L1102Amount")
+	private WebElementFacade l1102AmountInputField;
+	
+	@FindBy(id = "btn1102Gfe")
+	private WebElementFacade l1102GfeButton;
+	
+	@FindBy(id = "L1102BorrowerAmt")
+	private WebElementFacade l1102BorrowerAmountField;
+	
+	@FindBy(id = "L1102SellerAmt")
+	private WebElementFacade l1102SellerAmountInputField;
+	
+	@FindBy(id = "L1103Text")
+	private WebElementFacade l1103TextInputField;
+	
+	@FindBy(id = "btn1103Gfe")
+	private WebElementFacade l1103GfeButton;
+	
+	@FindBy(id = "L1103OverrideButton")
+	private WebElementFacade l1103ManualOverrideButton;
+	
+	@FindBy(id = "L1103BorrowerAmt")
+	private WebElementFacade l1103BorrowerAmountInputField;
+	
+	@FindBy(id = "L1103SellerAmt")
+	private WebElementFacade l1103SellerAmountInputField;
+	
+	@FindBy(id = "L1104Text")
+	private WebElementFacade l1104TextInputField;
+	
+	@FindBy(id = "L1104Amount")
+	private WebElementFacade l1104AmountInputField;
+	
+	@FindBy(id = "btn1104Gfe")
+	private WebElementFacade l1104GfeButton;
+	
+	@FindBy(id = "L1104OverrideButton")
+	private WebElementFacade l1104ManualOverrideButton;
+	
+	@FindBy(id = "L1104BorrowerAmt")
+	private WebElementFacade l1104BorrowerAmountField;
+	
+	@FindBy(id = "L1104SellerAmt")
+	private WebElementFacade l1104SellerAmountField;
+	
+	@FindBy(id = "premiumCalcButton")
+	private WebElementFacade premCalcButton;
+	
+	@FindBy(id = "L1105Amount")
+	private WebElementFacade l1105AmountInputField;
+	
+	@FindBy(id = "L1105BorrowerAmt")
+	private WebElementFacade l1105BorrowerAmountField;
+	
+	@FindBy(id = "L1105SellerAmt")
+	private WebElementFacade l1105SellerAmountField;
+	
+	@FindBy(id = "L1106Amount")
+	private WebElementFacade l1106AmountInputField;
+	
+	@FindBy(id = "L1106BorrowerAmt")
+	private WebElementFacade l1106BorrowerAmountField;
+	
+	@FindBy(id = "L1106SellerAmt")
+	private WebElementFacade l1106SellerAmountField;
+	
+	@FindBy(id = "L1107Text")
+	private WebElementFacade l1107TextInputField;
+	
+	@FindBy(id = "L1107Amount")
+	private WebElementFacade l1107AmountInputField;
+	
+	@FindBy(id = "L1107BorrowerAmt")
+	private WebElementFacade l1107BorrowerAmountField;
+	
+	@FindBy(id = "L1107SellerAmt")
+	private WebElementFacade l1107SellerAmountField;
+	
+	@FindBy(id = "L1108Text")
+	private WebElementFacade l1108TextInputField;
+	
+	@FindBy(id = "L1108Amount")
+	private WebElementFacade l1108AmountInputField;
+	
+	@FindBy(id = "L1108BorrowerAmt")
+	private WebElementFacade l1108BorrowerAmountField;
+	
+	@FindBy(id = "L1108SellerAmt")
+	private WebElementFacade l1108SellerAmountField;
+	
+	@FindBy(id = "L1109Text")
+	private WebElementFacade l1109TextField;
+	
+	@FindBy(id = "L1109Amount")
+	private WebElementFacade l1109AmountField;
+	
+	@FindBy(id = "btn1109Gfe")
+	private WebElementFacade l1109GfeButton;
+	
+	@FindBy(id = "L1109BorrowerAmt")
+	private WebElementFacade l1109BorrowerAmountField;
+	
+	@FindBy(id = "L1109SellerAmt")
+	private WebElementFacade l1109SellerAmountField;
+	
+	//TODO: section 1100 table
+	
+	@FindBy(id = "btnOpen1100Continuation")
+	private WebElementFacade add1100ContinuationButton;
+	
+	@FindBy(id = "btnOpenAddendum1100")
+	private WebElementFacade edit1100AddendumButton;
+	
 }
