da6453166a2664a4fabf9bcb97a2d7c20701013b
QT <ariordan@qualitestgroup.com>
2018-07-03 14:32:28 -0400

    adding methods for CD page objects

--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/closingdisclosure/page4/AdjustableInterestRateTableSection.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/closingdisclosure/page4/AdjustableInterestRateTableSection.java
@@ -1,9 +1,12 @@
 package com.catic.test.prepexpress.pages.createnewfile.closingdisclosure.page4;
 
+import java.math.BigDecimal;
+
 import org.openqa.selenium.support.FindBy;
 
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.webelements.Checkbox;
 
 public class AdjustableInterestRateTableSection extends PageObject {
 	@FindBy(id = "IndexTypeDescription")
@@ -35,4 +38,81 @@ public class AdjustableInterestRateTableSection extends PageObject {
 	
 	@FindBy(id = "AIRPrintTable")
 	private WebElementFacade printAIRTableOnPage4Checkbox;
+	
+	public String getIndexTypeDescription() {
+		return indexTypeDescriptionInputField.getValue();
+	}
+	
+	public void enterIndexTypeDescription(String description) {
+		indexTypeDescriptionInputField.type(description);
+	}
+	
+	public BigDecimal getMarginRate() {
+		return new BigDecimal(marginRatePercentInputField.getValue());
+	}
+	
+	public void enterMarginRate(BigDecimal rate) {
+		String value = rate.toPlainString();
+		marginRatePercentInputField.type(value);
+	}
+	
+	public BigDecimal getMinimumInterestRate() {
+		return new BigDecimal(minimumInterestRatePercentInputField.getValue());
+	}
+	
+	public void enterMinimumInterestRate(BigDecimal rate) {
+		String value = rate.toPlainString();
+		minimumInterestRatePercentInputField.type(value);
+	}
+	
+	public BigDecimal getMaximumInterestRate() {
+		return new BigDecimal(maximumInterestRatePercentInputField.getValue());
+	}
+	
+	public void enterMaximumInterestRate(BigDecimal rate) {
+		String value = rate.toPlainString();
+		maximumInterestRatePercentInputField.type(value);
+	}
+	
+	public String getFirstChangeMonthsCount() {
+		return interestRateChangeFrequencyFirstChangeInputField.getValue();
+	}
+	
+	public void enterFirstChangeMonthsCount(String count) {
+		interestRateChangeFrequencyFirstChangeInputField.type(count);
+	}
+	
+	public String getSubsequentChangesMonthsCount() {
+		return interestRateChangeFrequencySubsequentChangesInputField.getValue();
+	}
+	
+	public void enterSubsequentChangesMonthsCount(String count) {
+		interestRateChangeFrequencySubsequentChangesInputField.type(count);
+	}
+	
+	public BigDecimal getLimitOnInterestRateFirstChange() {
+		return new BigDecimal(interestRateLimitOnChangesFirstChangeInputField.getValue());
+	}
+	
+	public void enterLimitOnInterestRateFirstChange(BigDecimal rate) {
+		String value = rate.toPlainString();
+		interestRateLimitOnChangesFirstChangeInputField.type(value);
+	}
+	
+	public BigDecimal getLimitOnInterestRateSubsequentChanges() {
+		return new BigDecimal(interestRateLimitOnChangesSubsequentChangesInputField.getValue());
+	}
+	
+	public void enterLimitOnInterestRateSubsequentChanges(BigDecimal rate) {
+		String value = rate.toString();
+		interestRateLimitOnChangesSubsequentChangesInputField.type(value);
+	}
+	
+	public void setPrintAIRTableOnPage4(boolean value) {
+		setCheckbox(printAIRTableOnPage4Checkbox, value);
+	}
+	
+	public boolean printAIRTableOnPage4() {
+		return new Checkbox(printAIRTableOnPage4Checkbox).isChecked();
+	}
 }
--- src/test/java/com/catic/test/prepexpress/pages/createnewfile/closingdisclosure/page5/LoanCalculationsAndOtherDisclosuresSection.java
+++ src/test/java/com/catic/test/prepexpress/pages/createnewfile/closingdisclosure/page5/LoanCalculationsAndOtherDisclosuresSection.java
@@ -1,9 +1,13 @@
 package com.catic.test.prepexpress.pages.createnewfile.closingdisclosure.page5;
 
+import java.math.BigDecimal;
+
+import org.javamoney.moneta.Money;
 import org.openqa.selenium.support.FindBy;
 
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import util.MoneyUtil;
 
 public class LoanCalculationsAndOtherDisclosuresSection extends PageObject {
 	@FindBy(id = "TotalOfPayments")
@@ -26,4 +30,108 @@ public class LoanCalculationsAndOtherDisclosuresSection extends PageObject {
 	
 	@FindBy(xpath = "(//input[@name='liabilityProtectionGroup'])[2]")
 	private WebElementFacade stateLawDoesNotProtectFromLiabilityRadioButton;
+	
+	/**
+	 * Gets the value from the 'Total of Payments' field
+	 * @return the value
+	 */
+	public Money getTotalOfPayments() {
+		return MoneyUtil.fromString(totalOfPaymentsInputField.getValue());
+	}
+	
+	/**
+	 * Enters the given amount into the 'Total of Payments' field
+	 * @param amount
+	 */
+	public void enterTotalOfPayments(Money amount) {
+		totalOfPaymentsInputField.type(MoneyUtil.toString(amount));
+	}
+	
+	/**
+	 * Gets the value from the 'Finance Charge' field
+	 * @return the value
+	 */
+	public Money getFinanceCharge() {
+		return MoneyUtil.fromString(financeChargeInputField.getValue());
+	}
+	
+	/**
+	 * Enters the given amount into the 'Finance Charge' field
+	 * @param amount
+	 */
+	public void enterFinanceCharge(Money amount) {
+		financeChargeInputField.type(MoneyUtil.toString(amount));
+	}
+	
+	/**
+	 * Gets the value from the 'Amount Financed' field
+	 * @return the value
+	 */
+	public Money getAmountFinanced() {
+		return MoneyUtil.fromString(amountFinancedInputField.getValue());
+	}
+	
+	/**
+	 * Enters the given amount into the 'Amount Financed' field
+	 * @param amount
+	 */
+	public void enterAmountFinanced(Money amount) {
+		amountFinancedInputField.type(MoneyUtil.toString(amount));
+	}
+	
+	/**
+	 * Gets the value from the 'Annual Percentage Rate (APR)' field
+	 * @return the value
+	 */
+	public BigDecimal getAnnualPercentageRate() {
+		return new BigDecimal(annualPercentageRateInputField.getValue());
+	}
+	
+	/**
+	 * Enters the given value into the 'Annual Percentage Rate (APR)' field
+	 * @param rate
+	 */
+	public void enterAnnualPercentageRate(BigDecimal rate) {
+		String value = rate.toPlainString();
+		annualPercentageRateInputField.type(value);
+	}
+	
+	/**
+	 * Gets the value from the 'Total Interest Percentage' field
+	 * @return the value
+	 */
+	public BigDecimal getTotalInterestRatePercentage() {
+		return new BigDecimal(totalInterestPercentageInputField.getValue());
+	}
+	
+	/**
+	 * Enters the given value into the 'Total Interest Percentage' field
+	 * @param rate
+	 */
+	public void enterTotalInterestRatePercentage(BigDecimal rate) {
+		String value = rate.toPlainString();
+		totalInterestPercentageInputField.type(value);
+	}
+	
+	/**
+	 * Returns whether the 'State Law may Protect from Liability' option is selected
+	 * @return {@code true} if it is selected, {@code false} otherwise
+	 */
+	public boolean stateLawMayProtectFromLiability() {
+		return stateLawMayProtectFromLiabilityRadioButton.isSelected();
+	}
+	
+	/**
+	 * If {@code true}, then selects the 'State law may protect from liability' option.
+	 * Otherwise, selects the 'State law does not protect from liability' option
+	 * @param value
+	 */
+	public void setStateLawMayProtectFromLiability(boolean value) {
+		if (value) {
+			stateLawMayProtectFromLiabilityRadioButton.click();
+		}
+		else {
+			stateLawDoesNotProtectFromLiabilityRadioButton.click();
+		}
+	}
 }
